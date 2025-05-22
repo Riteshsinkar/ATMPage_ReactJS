687ef352582f273e09bccbd7a29a56cc7a0f3207
Riteshsinkar <riteshsinkar29@gmail.com>
2025-05-22 22:00:47 +0530

    files added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+
+import Card from './Card'
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Card/>
+    
+    </>
+  );
+}
+
+export default App;
